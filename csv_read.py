82af24d4cac533120314325ff340f783087637d7
mikhailtorzhok <spartacusxxxxxx@gmail.com>
2023-01-01 13:32:30 +0300

    some wrapped def was written

--- csv_read.py
+++ csv_read.py
@@ -21,12 +21,9 @@ Trash_dictionary = {}
 
 def main():
     print("Hello World!")
-    #firefox_options = Options()
     chrome_options = Options()
     chrome_options.add_experimental_option("detach", True)
-    #driver = webdriver.Firefox(options=firefox_options)
     driver = webdriver.Chrome(options=chrome_options) 
-    #driver.maximize_window()
     driver.set_window_size(1920, 1080)
     driver.get("https://torsed.voskhod.ru/app/#!")
 
@@ -42,93 +39,59 @@ def main():
         
         button_login = driver.find_element(By.XPATH, '//div[@class="v-button v-widget cuba-login-submit v-button-cuba-login-submit v-has-width"]')
         button_login.click()
-        
-        #button_spravochnikiiiii = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[contains(text(), "Справочники")]')))
-        
-        #WebDriverWait wait = new WebDriverWait(driver, 3)
-        #wait.until(ExpectedConditions.elementToBeClickable(By.XPATH, '//span[@class="v-menubar-menuitem-caption"]'))
-        #WebDriverWait(driver, delay).until(EC.element_to_be_clickable('//span[@class="v-menubar-menuitem-caption"]'))
-        
-        
-        #button_spravochniki = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//span[@class="v-menubar-submenu-indicator"]'))) #exeption 
+   
         button_spravochniki = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[text()="Справочники"]')))
         button_spravochniki = driver.find_element(By.CSS_SELECTOR, "span[class='v-menubar-menuitem']")
-        #button_spravochniki = driver.find_element(By.XPATH, "//*[contains(@div,'Справочники')]")
-        
-        #select = Select(driver.find_element(By.XPATH, '//span[@class="v-menubar-menuitem"]'))
-        #select.select_by_visible_text('Справочники')
+ 
         print ("Page is ready!")
-        #button_spravochniki = driver.find_element(By.XPATH, '//span[@class="v-menubar-menuitem-caption"]')
-
-
-        #button_spravochniki.click()
 
         button_spravochniki = driver.find_element(By.CSS_SELECTOR, '.cuba-main-menu.v-menubar')
 
-
-       
         print ("tab_names " + button_spravochniki.text)
-        #print ("tab_names " + button_spravochniki.location_once_scrolled_into_view)
-        #print ("tab_names " + button_spravochniki.location_once_scrolled_into_view)
-        
-        #button_spravochniki = driver.find_element(By.ID, 'span')
-        #button_spravochniki.click()
-
-        #parent_page1 = driver.find_element((By.NAME, "Справочники"))  
-
-
+ 
         button_spravochniki = driver.find_element(By.CSS_SELECTOR, "div[tabindex='0']")
-        #button_spravochniki.select_by_visible_text('Справочники')
-        #selectedOption = Select(driver.find_element(By.CSS_SELECTOR, "div[tabindex='0']"))
-        #roleOptions = driver.find_element(By.XPATH, "v-button v-widget cuba-login-submit v-button-cuba-login-submit v-has-width")[0]
 
-        #roleOptions.click()
-
-        
         button_spravochniki = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div/div/div/div/div[1]/div/div/div[3]/div/span[5]")
         action = ActionChains(driver)
         action.move_to_element(button_spravochniki)
         action.click(button_spravochniki)
         action.perform()
 
-        button_Kontragenty = driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div/div/span[6]/span[1]")
-        action = ActionChains(driver)
-        action.move_to_element(button_Kontragenty)
-        action.click(button_Kontragenty)
-        action.perform()
-                                                                                
-        button_Ur_litca = driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div/div/span[1]/span[1]")
-        action = ActionChains(driver)
-        action.move_to_element(button_Ur_litca)
-        action.click(button_Ur_litca)
-        action.perform()
+        #button_Kontragenty
+        find_and_click_element_by_path(driver, delay, '/html/body/div[3]/div[2]/div/div/span[6]/span[1]')
+        #button_Kontragenty = driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div/div/span[6]/span[1]")
+        #action = ActionChains(driver)
+        #action.move_to_element(button_Kontragenty)
+        #action.click(button_Kontragenty)
+        #action.perform()
 
+        #button_Ur_litca 
+        find_and_click_element_by_path(driver, delay, '/html/body/div[3]/div[3]/div/div/span[1]/span[1]')                                                                   
+        #button_Ur_litca = driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div/div/span[1]/span[1]")
+        #action = ActionChains(driver)
+        #action.move_to_element(button_Ur_litca)
+        #action.click(button_Ur_litca)
+        #action.perform()
 
-        button_create_new = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/div/div/div[2]/div/div/div/div/div/div/div/div/div[3]/div/div[1]/div/div[1]/div/div[1]/div')))
-        action = ActionChains(driver)
-        action.move_to_element(button_create_new)
-        action.click(button_create_new)
-        action.perform()
-
-
-        #driver.execute_script("arguments[0].click();", button_spravochniki)
-        
-
-        #print ("button_spravochniki is ready!")
-        #print (button_spravochniki)
-        #print (button_spravochniki.get_property("text_length"))
-        #option = button_spravochniki.get_property('class')
-
-        #for option in button_spravochniki:
-            #print("Value is: %s" % option.get_attribute("value"))
-            #option.click()
-
-
-
-        
+        #button_create_new
+        find_and_click_element_by_path(driver, delay, '/html/body/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/div/div/div[2]/div/div/div/div/div/div/div/div/div[3]/div/div[1]/div/div[1]/div/div[1]/div')
+        #button_create_new = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/div/div/div[2]/div/div/div/div/div/div/div/div/div[3]/div/div[1]/div/div[1]/div/div[1]/div')))
+        #action = ActionChains(driver)
+        #action.move_to_element(button_create_new)
+        #action.click(button_create_new)
+        #action.perform()
+    
     except TimeoutException:
         print ("Loading took too much time!")
     
+
+def find_and_click_element_by_path(driver, delay, path):
+    button = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, path)))
+    action = ActionChains(driver)
+    action.move_to_element(button)
+    action.click(button)
+    action.perform()
+    return
 
 
 def write_csv(data, name):
